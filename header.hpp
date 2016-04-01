73fc107cabd5c7877cd71bab08ea5696c0dc3638
Volkov German <Volkov.german.1997@gmail.com>
2016-04-01 20:19:37 +0300

    AlertSubSystem Interface

---
+++ header.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+/************************************ LIBRARIES ************************************/
+
+#include <iostream>	
+#include <ctime>
+
+using namespace std;
+
+
+/************************************ INTERFACE ************************************/
+
+// Получение данных из config.json
+int get_origin(string);
+int get_type(string);
+
+// Создать alert
+int put_alert(int origin, int type, string json_subkey);
+
+// Получить alerts
+int get_alert(int origin, int type, string json_subkey);
+int get_alerts(int origin, int type);
+int get_alerts(int origin);
+
+// Удалить алерт
+int remove_alert(int origin, int type, string json_subkey);
+int remove_alerts(int origin, int type);
+int remove_alerts(int origin);
+
+// Перевод alert в CLEARED
+int clear_alert(int key);
+
+
+/************************************ BACKEND ************************************/
+
+// Сохранить alert
+int save_alert(int origin, int type, string json_subkey);
+
+// Игнорировать alert на время ignore_time
+int ignore_alert(int origin, int type, string json_subkey, time_t ignore_time);
+  
---
+++ implemetation.cpp
@@ -0,0 +1,61 @@
+#include "header.hpp"
+
+int get_origin(string)
+{
+  return 0;
+}
+
+int get_type(string)
+{
+  return 0;
+}
+
+int put_alert(int origin, int type, string json_subkey)
+{
+  return 0;
+}
+
+int get_alert(int origin, int type, string json_subkey)
+{
+  return 0;
+}
+
+int get_alerts(int origin, int type)
+{
+  return 0;
+}
+
+int get_alerts(int origin)
+{
+  return 0;
+}
+
+int remove_alert(int origin, int type, string json_subkey)
+{
+  return 0;
+}
+
+int remove_alerts(int origin, int type)
+{
+  return 0;
+}
+
+int remove_alerts(int origin)
+{
+  return 0;
+}
+
+int clear_alert(int key)
+{
+  return 0;
+}
+
+int save_alert(int origin, int type, string json_subkey)
+{
+  return 0;
+}
+
+int ignore_alert(int origin, int type, string json_subkey, time_t ignore_time)
+{
+  return 0;
+}
