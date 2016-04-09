8797a91b864f125e3e16204705ece6630e4cae67
Evgenii <zhurko.e96@gmail.com>
2016-04-09 22:41:30 +0300

    [MapReduce]Create map interface
    Change-Id: I5c4d0430ec2bba1931d736dbb7af7d1d0be53be4

---
+++ utils/TaskInfo.hpp
@@ -0,0 +1,31 @@
+#ifndef SSODC_UTILS_TASKINFO_HPP_
+#define SSODC_UTILS_TASKINFO_HPP_
+
+#include <string>
+
+namespace ssodc {
+namespace utils {
+	
+class taskinfo {
+private:
+    unsigned int m_id;
+    unsigned int m_type;
+    std::string m_datapath;
+    std::string m_codepath;
+public:
+    taskinfo();
+    taskinfo(int, int, std::string, std::string);
+    taskinfo(const taskinfo&);
+    ~taskinfo();
+
+    taskinfo& operator=(const taskinfo&);
+    unsigned int getId();
+    unsigned int getType();
+    std::string getDataPath();
+    std::string getCodePath();
+    int getCodePath(std::string&);
+};
+}
+}
+
+#endif //SSODC_UTILS_TASKINFO_HPP_
