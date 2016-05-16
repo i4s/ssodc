a2eacf17b7bb471b608cd17c4a1e9eadf4a73633
Evgenii <zhurko.e96@gmail.com>
2016-05-17 02:06:40 +0300

    [UTILS]Implemented class to work with files(get length, generate name)
    Change-Id: Iad1dac7d3aca9f43e0bd568d250263ceab4c24a8
    Reviewed-on: https://review.gerrithub.io/276870
    Reviewed-by: Erick Mizhevich <mimizherick@gmail.com>
    Tested-by: Evgenii Zhurko <zhurko.e96@gmail.com>

---
+++ utils/FileWorker.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+namespace ssodc {
+namespace utils {
+
+const std::string DIRECTORY_TO_SAVE = "/mnt/ssodcdata/";
+const std::string TXT_FILE_EXTENSION = ".txt";
+
+class FileWorker {
+public:
+    static u_int64_t FileLenght(std::string&);
+
+    static std::string GenerateFileName();
+    static std::string GenerateFileName(int);
+};
+
+} /* namespace utils */
+} /* namespace ssodc */
