122ba8edf21ebc9e24f7ca9200daa6a675fd03b6
Evgenii Zhurko <zhurko.e96@gmail.com>
2016-05-20 21:31:31 +0300

    [UTILS]Added converter taskinfo to json
    Change-Id: I0f57a6ea9d72e73278a2ea3265338af9d4fc162b

--- utils/JSONConverter.cpp
+++ utils/JSONConverter.cpp
@@ -3,6 +3,40 @@
 namespace ssodc {
 namespace utils {
 
+int JSONConverter::GetRequestType(std::string& inputJson) {
+    Json::Value root;
+    Json::Reader reader;
+    if(!reader.parse(inputJson, root)) {
+        //Logging, where are you?
+    }
+    return root[REQUEST_TYPE].asInt();
+}
+
+std::string JSONConverter::GetMapKeyType(std::string& inputJson) {
+    Json::Value root;
+    Json::Reader reader;
+    if(!reader.parse(inputJson, root)) {
+        //TODO Logging
+    }
+    return root[KEY_TYPE].asString();
+}
+
+std::string JSONConverter::GetMapValueType(std::string& inputJson) {
+    Json::Value root;
+    Json::Reader reader;
+    if(!reader.parse(inputJson, root)) {
+        //TODO Logging
+    }
+    return root[VALUE_TYPE].asString();
+}
+
+int JSONConverter::CreateSimpleRequest(int request, std::string& outputJson) {
+    Json::Value root;
+    root[REQUEST_TYPE] = request;
+    Json::StyledWriter writer;
+    outputJson = writer.write(root);
+    return 0;
+}
 
 template<>
 int JSONConverter::StringToMap<int, int>(std::string& inputJson,
@@ -10,9 +44,9 @@ int JSONConverter::StringToMap<int, int>(std::string& inputJson,
     Json::Value root;
     Json::Reader reader;
     if(!reader.parse(inputJson, root)) {
-        //Logging, where are you?
+        //TODO Logging
     }
-    for(auto r: root) {
+    for(auto r: root[DATA]) {
         outputMap.insert(std::pair<int, int>(r[MAP_KEY].asInt(), r[MAP_VALUE].asInt()));
     }
     return 0;
@@ -24,9 +58,9 @@ int JSONConverter::StringToMap<int, std::string>(std::string& inputJson,
     Json::Value root;
     Json::Reader reader;
     if(!reader.parse(inputJson, root)) {
-        //Logging, where are you?
+        //TODO Logging
     }
-    for(auto r: root) {
+    for(auto r: root[DATA]) {
         outputMap.insert(std::pair<int, std::string>(r[MAP_KEY].asInt(),
                          r[MAP_VALUE].asString()));
     }
@@ -34,8 +68,9 @@ int JSONConverter::StringToMap<int, std::string>(std::string& inputJson,
 }
 
 int JSONConverter::MapWithVectorValueToString(std::map<int, std::vector<int>>& inputMap,
-        std::string& outputJson) {
+        int request, std::string& outputJson) {
     Json::Value root;
+    Json::Value jsonMap;
     std::map<int, std::vector<int>>::const_iterator it = inputMap.begin();
     std::map<int, std::vector<int>>::const_iterator end = inputMap.end();
     for (; it != end; it++) {
@@ -47,8 +82,12 @@ int JSONConverter::MapWithVectorValueToString(std::map<int, std::vector<int>>& i
         }
         currentElement[MAP_KEY] = it->first;
         currentElement[MAP_VALUE] = jsonVector;
-        root.append(currentElement);
+        jsonMap.append(currentElement);
     }
+    root[REQUEST_TYPE] = request;
+    root[KEY_TYPE] = typeid(inputMap.begin()->first).name();
+    root[VALUE_TYPE] = typeid((inputMap.begin()->second).back()).name();
+    root[DATA] = jsonMap;
     Json::StyledWriter writer;
     outputJson = writer.write(root);
     return 0;
@@ -59,9 +98,9 @@ int JSONConverter::StringToMapWithVector(std::string& inputJSON,
     Json::Value root;
     Json::Reader reader;
     if(!reader.parse(inputJSON, root)) {
-        //Logging, where are you?
+        //TODO Logging
     }
-    for(auto pair: root) {
+    for(auto pair: root[DATA]) {
         std::vector<int> vector;
         for(auto vectorElement: pair[MAP_VALUE]) {
             vector.push_back(std::stoi(vectorElement.asString()));
@@ -71,6 +110,32 @@ int JSONConverter::StringToMapWithVector(std::string& inputJSON,
     return 0;
 }
 
+int JSONConverter::CreateNewTask(TaskInfo& taskInfo, int request, std::string& outputJson) {
+    Json::Value root;
+    root[REQUEST_TYPE] = request;
+    root[TI_ID] = taskInfo.GetId();
+    root[TI_TYPE] = taskInfo.GetType();
+    root[TI_CODE_PATH] = taskInfo.GetCodePath();
+    root[TI_DATA_PATH] = taskInfo.GetDataPath();
+    root[TI_EXECUTABLE_PATH] = taskInfo.GetExecutablePath();
+    Json::StyledWriter writer;
+    outputJson = writer.write(root);
+    return 0;
+}
+
+int JSONConverter::GetNewTask(std::string& inputJSON, ssodc::utils::TaskInfo& taskInfo) {
+    Json::Value root;
+    Json::Value jsonTaskInfo;
+    Json::Reader reader;
+    if(!reader.parse(inputJSON, root)) {
+        //TODO Logging
+    }
+    taskInfo = ssodc::utils::TaskInfo(root[TI_ID].asInt(),
+                                      static_cast<ssodc::utils::TaskType>(root[TI_TYPE].asInt()),
+                                      root[TI_DATA_PATH].asString(), root[TI_CODE_PATH].asString(),
+                                      root[TI_EXECUTABLE_PATH].asString());
+    return 0;
+}
 
 } /* namespace utils */
 } /* namespace ssodc */
--- utils/JSONConverter.hpp
+++ utils/JSONConverter.hpp
@@ -3,28 +3,52 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <typeinfo>
 #include <jsoncpp/json/json.h>
+
+#include "TaskInfo.hpp"
 
 namespace ssodc {
 namespace utils {
 
+const std::string DATA = "Data";
 const std::string MAP_KEY = "Key";
 const std::string MAP_VALUE = "Value";
+const std::string KEY_TYPE = "KeyType";
+const std::string VALUE_TYPE = "ValueType";
+const std::string REQUEST_TYPE = "Request";
+
+const std::string TI_ID = "TaskInfoID";
+const std::string TI_TYPE = "TaskInfoType";
+const std::string TI_CODE_PATH = "TaskInfoCodePath";
+const std::string TI_DATA_PATH = "TaskInfoDataPath";
+const std::string TI_EXECUTABLE_PATH = "TaskInfoExecutablePath";
 
 class JSONConverter
 {
 public:
-    template<class Key, class Value>
-    static int MapToString(std::map<Key, Value>&, std::string&);
-    template<class Key, class Value>
+    static int GetRequestType(std::string&);
+    static std::string GetMapKeyType(std::string&);
+    static std::string GetMapValueType(std::string&);
+    static int GetNewTask(std::string&, ssodc::utils::TaskInfo&);
+
+    static int CreateSimpleRequest(int, std::string&);
+    static int CreateNewTask(TaskInfo&, int, std::string&);
+
+    template <class Key, class Value>
+    static int MapToString(std::map<Key, Value>&, int, std::string&);
+    template <class Key, class Value>
     static int StringToMap(std::string&, std::map<Key, Value>&);
     static int StringToMapWithVector(std::string&, std::map<int, std::vector<int>>&);
-    static int MapWithVectorValueToString(std::map<int, std::vector<int>>&, std::string&);
+    static int MapWithVectorValueToString(std::map<int, std::vector<int>>&,
+                                          int, std::string&);
 };
 
 template<class Key, class Value>
-int JSONConverter::MapToString(std::map<Key, Value>& inputMap, std::string& outputJson) {
+int JSONConverter::MapToString(std::map<Key, Value>& inputMap,
+                               int request, std::string& outputJson) {
     Json::Value root;
+    Json::Value jsonMap;
     typename std::map<Key, Value>::const_iterator it = inputMap.begin();
     typename std::map<Key, Value>::const_iterator end = inputMap.end();
     for ( ; it != end; ++it) {
@@ -32,8 +56,12 @@ int JSONConverter::MapToString(std::map<Key, Value>& inputMap, std::string& outp
         Json::Value jsonVector;
         currentElement[MAP_KEY] = it->first;
         currentElement[MAP_VALUE] = it->second;
-        root.append(currentElement);
+        jsonMap.append(currentElement);
     }
+    root[REQUEST_TYPE] = request;
+    root[KEY_TYPE] = typeid(inputMap.begin()->first).name();
+    root[VALUE_TYPE] = typeid(inputMap.begin()->second).name();
+    root[DATA] = jsonMap;
     Json::StyledWriter writer;
     outputJson = writer.write(root);
     return 0;
