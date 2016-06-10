24f7c383eaf8eb5e799f55c6a2da6fdd1b47ea99
Evgenii Zhurko <zhurko.e96@gmail.com>
2016-06-10 23:38:42 +0300

    [UTILS]Implemented source builder for windows
    Change-Id: I293dd5f6a92c5e82f81b8ad41a2f469024802ab8
    Reviewed-on: https://review.gerrithub.io/279982
    Reviewed-by: Evgenii Zhurko <zhurko.e96@gmail.com>
    Tested-by: Evgenii Zhurko <zhurko.e96@gmail.com>

--- utils/SourceBuilder.cpp
+++ utils/SourceBuilder.cpp
@@ -5,10 +5,10 @@
 namespace ssodc {
 namespace utils {
 
-int SourceBuilder::Build(std::string& fileNames, std::string& outputFileName) {
+int SourceBuilder::BuildLinux(std::string& fileNames, std::string& outputFileName) {
     char buffer[BUFFER_SIZE];
     std::string outputMessage = "";
-    std::string command = GenerateCommand(fileNames, outputFileName);
+    std::string command = GenerateCommandLinux(fileNames, outputFileName);
     std::shared_ptr<FILE> compiler(popen(command.c_str(), "r"), pclose);
     if(!compiler) {
         //TODO: Add logging
@@ -22,8 +22,33 @@ int SourceBuilder::Build(std::string& fileNames, std::string& outputFileName) {
     return 0;
 }
 
-std::string SourceBuilder::GenerateCommand(std::string& fileNames, std::string& outputFileName) {
+int SourceBuilder::BuildWin(std::string& fileNames, std::string& outputFileName) {
+    char buffer[BUFFER_SIZE];
+    std::string outputMessage = "";
+    std::string command = GenerateCommandWin(fileNames, outputFileName);
+    std::shared_ptr<FILE> compiler(popen(command.c_str(), "r"), pclose);
+    if(!compiler) {
+        //TODO: Add logging
+        return -1;
+    }
+    while (!feof(compiler.get())) {
+        if (fgets(buffer, BUFFER_SIZE, compiler.get()) != NULL) {
+            outputMessage += buffer;
+        }
+    }
+    return 0;
+}
+
+std::string SourceBuilder::GenerateCommandLinux(std::string& fileNames, std::string& outputFileName) {
     std::string command = "/usr/bin/g++ 2>&1 -std=c++14 ";
+    command.append(fileNames);
+    command.append(" -o ");
+    command.append(outputFileName);
+    return command;
+}
+
+std::string SourceBuilder::GenerateCommandWin(std::string& fileNames, std::string& outputFileName) {
+    std::string command = "/usr/bin/i586-mingw32msvc-g++ 2>&1-std=c++14 ";
     command.append(fileNames);
     command.append(" -o ");
     command.append(outputFileName);
